a4ff0cfdc5249917dfa5f206ee40e5183bbd5f92
Deadpool042 <laurent@puleri.com>
2024-10-18 14:31:53 +0200

    correction auto build V2

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export"
+  output: "export",
+  assetPrefix: "/admin/",
+  basePath: "/admin"
 };
 
 export default nextConfig;
