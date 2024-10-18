cf372138daeb2c2c16e273f8f0d74de0315d620c
Deadpool042 <laurent@puleri.com>
2024-10-18 13:58:16 +0200

    correction lien + logov3

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  assetPrefix: "/admin/",
-  basePath: "/admin"
+  output: "export"
 };
 
 export default nextConfig;
